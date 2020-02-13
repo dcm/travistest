f4b63daf624397c3df38c712dd94772a7419e802
David C. Morse <David.Morse@morganstanley.com>
2020-02-13 12:08:24 +0000

    add script

---
+++ do_things.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "'which python2' under $(python --version)"
+which python2 || true
+
+echo "'which python3' under $(python --version)"
+which python3 || true
+
+echo "printing entire PATH"
+OIFS="$IFS"
+IFS=":"
+for d in $PATH: do
+    echo ">>> Contents of $d:"
+    ls -Flash "$d" | sed -E 's/^/\t/' || true
+done
+
+
